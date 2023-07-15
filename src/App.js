d54689ec2f313d0c4f3796ef6554ca1aaab2f7e5
GustavoDimeira <gustavodimeira1@gmail.com>
2023-07-15 19:27:48 -0400

    feat: page about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-import Navegation from "./pages/Navegation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import {
+  Navegation,
+  About,
+} from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <Navegation />
+    <div className="container">
+      <BrowserRouter>
+        <Navegation className="nav" />
+        <div className="content">
+          <Routes>
+            <Route path='/about' element={ <About /> } />
+            <Route path='/projects' element={ <About /> } />
+            <Route path='/skills' element={ <About /> } />
+            <Route path='/exemples'>
+              <Route path='/exemples/login' element={ <About /> } />
+              <Route path='/exemples/register' element={ <About /> } />
+              <Route path='/exemples/products' element={ <About /> } />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Navegation from "./Navegation";
+import About from "./About";
+// import Inicial from "./Inicial";
+// import Projects from "./Projects";
+// import Knowledge from "./Knowledge";
+// import Exemples from "./Exemples";
+
+export { Navegation, About };
